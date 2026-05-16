d8379af21ec0cc46fa64d334d1c14b931e50df59
Gael Levesque <gaellevesque535@gmail.com>
2026-05-16 18:04:35 -0400

    fix white page

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [viteReact()],
+  plugins: [react()],
+  base: '/',
 })
